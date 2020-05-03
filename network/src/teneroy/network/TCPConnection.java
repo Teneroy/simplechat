ff98af14b5d2544d64a6aaa11d8e686ce44b96f3
Teneroy <alexchernishof@gmail.com>
2020-05-04 02:18:31 +0300

    Added tcp connection listener and some tcp class implementation

---
+++ network/src/teneroy/network/TCPConnection.java
@@ -0,0 +1,61 @@
+package teneroy.network;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class TCPConnection {
+
+    private final Socket socket;
+    private final Thread rxThread; // Thread that is listening input connection
+    private final BufferedReader in;
+    private final BufferedWriter out;
+    private final TCPConnectionListener eventListener;
+
+    public TCPConnection(Socket socket, TCPConnectionListener eventListener) throws IOException {
+        this.socket = socket;
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+        this.eventListener = eventListener;
+        rxThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    eventListener.onConnectionReady(TCPConnection.this);
+                    while (!rxThread.isInterrupted()) { //While thread is not interrupted, listening string reception
+                        eventListener.onRecieveString(TCPConnection.this, in.readLine());
+                    }
+                } catch (IOException e) {
+                    eventListener.onException(TCPConnection.this, e);
+                } finally {
+                    eventListener.onDisconnect(TCPConnection.this);
+                }
+            }
+        });
+        rxThread.start();
+    }
+
+    public synchronized void sendString(String value) {
+        try {
+            out.write(value + "\r\n");
+            out.flush();
+        } catch (IOException e) {
+            eventListener.onException(TCPConnection.this, e);
+            disconnect(); //If something went wrong, disconnect
+        }
+    }
+
+    public synchronized void disconnect() {
+        rxThread.interrupt();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            eventListener.onException(TCPConnection.this, e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "TCPConnection: " + socket.getInetAddress() + ": " + socket.getPort();
+    }
+}
